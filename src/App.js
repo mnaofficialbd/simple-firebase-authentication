cbca674ae75bdfe641cb2445f4f1e2967cf29731
mnaofficial <mna.official92@gmail.com>
2022-04-07 00:18:06 +0600

    login and logOut

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import './App.css';
 import app from './firebase.init';
-import {getAuth, GoogleAuthProvider, signInWithPopup} from "firebase/auth"
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth"
+import { useState } from 'react';
 
-const auth=getAuth(app)
+const auth = getAuth(app)
 
 function App() {
+
+  const [user, setUser] = useState({})
   const provider = new GoogleAuthProvider();
-  const handleGoogleSignIn=()=>{
-    signInWithPopup(auth,provider)
-    .then(result=>{
-      const user=result.user;
-      console.log(user);
-    })
-    .catch(error=>{
-      console.log(error);
-    })
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+      .then(result => {
+        const user = result.user;
+        setUser(user);
+        console.log(user);
+      })
+      .catch(error => {
+        console.log(error);
+      })
   }
+
+  const handleGoogleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({})
+      })
+      .catch(error => {
+        setUser({})
+      })
+  }
+
   return (
     <div className="App">
       <h2>Simple firebase Authentication</h2>
-      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      {/* {condition ? true : false} */}
+      {
+        user.email ? <button onClick={handleGoogleSignOut}>Sign out</button>
+          :
+          <button onClick={handleGoogleSignIn}>Google Sign In</button>
+
+      }
+
+      <h3>User Name: {user.displayName}</h3>
+      <img src={user.photoURL} alt="" />
     </div>
   );
 }
